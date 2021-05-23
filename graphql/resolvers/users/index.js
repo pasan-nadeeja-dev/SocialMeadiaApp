b399e0f32b7f9f31d219666baa48abd2324d7533
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2021-05-23 23:14:47 +0530

    Add user login resolver

--- graphql/resolvers/users/index.js
+++ graphql/resolvers/users/index.js
@@ -1,7 +1,9 @@
 const { userRegister } = require("./register");
+const { userLogin } = require("./login");
 
 module.exports = {
   Mutation: {
     register: userRegister,
+    login: userLogin,
   },
 };
