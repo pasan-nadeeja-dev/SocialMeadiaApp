a7e1440e9d8b20af23048319c51ddfea925aaf9e
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2021-05-23 23:14:35 +0530

    Add user login input validator

--- util/validator.js
+++ util/validator.js
@@ -1,3 +1,17 @@
+function validateUsername(username, errors) {
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  return;
+}
+
+function validateResponse(errors) {
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+}
+
 module.exports.registerInputValidator = (
   username,
   password,
@@ -12,7 +26,9 @@ module.exports.registerInputValidator = (
   //empty and format of email validation
   if (email.trim() === "") {
     errors.email = "Email must not be empty";
-  } else if (!email.match(/^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/)) {
+  } else if (
+    !email.match(/^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/)
+  ) {
     errors.email = "Email must be in valid format";
   }
   //password length validation
@@ -26,4 +42,16 @@ module.exports.registerInputValidator = (
     errors,
     valid: Object.keys(errors).length < 1,
   };
+};
+
+module.exports.loginInputValidator = (username, password) => {
+  const errors = {};
+  validateUsername(username, errors);
+
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  } else if (password.length < 8 && password.length > 15) {
+    errors.password = "Password length must be between 8 and 15";
+  }
+  return validateResponse(errors);
 };
