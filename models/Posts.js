9700aa54cc492b554dbb71647f53f1f92fba13de
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2021-09-21 21:30:34 +0530

    update post schema

--- models/Posts.js
+++ models/Posts.js
@@ -4,7 +4,23 @@ const postSchema = new Schema({
   body: String,
   username: String,
   createdAt: String,
-  
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String,
+    },
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String,
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+  },
 });
 
 module.exports = model("Posts", postSchema);
